a926e7adab65cd17ee3c9655bebfab4651834b18
uuranus <hyur0920@gmail.com>
2023-03-23 23:09:41 +0900

    [feat] 로그인 아이디, 비밀번호 입력 데이터바인딩

--- app/src/main/java/com/vocaengplus/vocaengplus/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/vocaengplus/vocaengplus/ui/login/LoginViewModel.kt
@@ -1,36 +1,49 @@
 package com.vocaengplus.vocaengplus.ui.login
 
-import android.widget.EditText
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.vocaengplus.vocaengplus.R
+import androidx.lifecycle.viewModelScope
 import com.vocaengplus.vocaengplus.ui.util.Validation
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
 
-    val id = MutableStateFlow("")
-    val password = MutableStateFlow("")
+    val id = MutableLiveData("")
+    val password = MutableLiveData("")
 
-    private val _idErrorText = MutableStateFlow("")
-    val idErrorText:StateFlow<String> get() = _idErrorText
+    private val _idErrorText = MutableLiveData("")
+    val idErrorText:LiveData<String> get() = _idErrorText
 
     private val _passwordErrorText = MutableStateFlow<String>("")
-    val passwordErrorText:StateFlow<String> get() = _idErrorText
+    val passwordErrorText:StateFlow<String> get() = _passwordErrorText
 
     private val _snackBarMessage = MutableStateFlow("")
     val snackBarMessage: StateFlow<String> get() = _snackBarMessage
 
+
+    fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        println("!!!!!!!")
+    }
+
+
     fun onIdChanged() {
-        val inputId = id.value
-        println("inputId $inputId")
-        if (Validation.isValidateEmail(inputId).not()) {
-            _idErrorText.value = "아이디가 올바르지 않습니다"
+
+        println("onIdChanged")
+        val idValue = id.value.takeIf { it.isNullOrEmpty().not() } ?: return
+
+        if (Validation.isValidateEmail(idValue).not()) {
+            _idErrorText.value = "5~20자의 영문 소문자, 숫자와 특수기호_,- 만 사용 가능합니다"
+        } else {
+            _idErrorText.value =  ""
         }
     }
 
     fun onPasswordChanged(){
-        val inputPassword = password.value
+        val inputPassword = password.value ?: ""
         println("password $inputPassword")
         if(Validation.isValidatePassword(inputPassword).not()){
             _passwordErrorText.value = "비밀번호가 올바르지 않습니다"
